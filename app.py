54c11ffbf1564c741c3d3ef02bd2cd00fe8996df
kiamaaa <victorkiama23@gmail.com>
2026-06-26 16:18:04 +0300

    Add POST, PATCH, and DELETE routes for events

--- app.py
+++ app.py
@@ -19,36 +19,42 @@ events = [
 
 # TODO: Task 1 - Define the Problem
 # Create a new event from JSON input
+
 @app.route("/events", methods=["POST"])
 def create_event():
-    # TODO: Task 2 - Design and Develop the Code
+    data = request.get_json()
+    new_id = max([e.id for e in events]) + 1 if events else 1
+    new_event = Event(id = new_id, title = data["title"])
+    events.append(new_event)
+    return jsonify(new_event.to_dict()),201
 
-    # TODO: Task 3 - Implement the Loop and Process Each Element
-
-    # TODO: Task 4 - Return and Handle Results
-    pass
-
+    
+   
+    
 # TODO: Task 1 - Define the Problem
 # Update the title of an existing event
 @app.route("/events/<int:event_id>", methods=["PATCH"])
 def update_event(event_id):
-    # TODO: Task 2 - Design and Develop the Code
-
-    # TODO: Task 3 - Implement the Loop and Process Each Element
-
-    # TODO: Task 4 - Return and Handle Results
-    pass
-
-# TODO: Task 1 - Define the Problem
-# Remove an event from the list
+    data = request.get_json()
+    event =next((e for e in events if event_id == e.id),None)
+    if not event:
+        return "Event not found",404
+    if "title" in data:
+        event.title = data["title"]
+        return jsonify(event.to_dict()),200
+    if "title" not in data:
+        return "No title found",400
+    
+    
 @app.route("/events/<int:event_id>", methods=["DELETE"])
 def delete_event(event_id):
-    # TODO: Task 2 - Design and Develop the Code
-
-    # TODO: Task 3 - Implement the Loop and Process Each Element
-
-    # TODO: Task 4 - Return and Handle Results
-    pass
+    global events
+    event = next((e for e in events if event_id == e.id ),None)
+    if not event:
+        return("Event not found",404)
+    events = [e for e in events if event_id != e.id]
+    return "Event deleted",204
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
